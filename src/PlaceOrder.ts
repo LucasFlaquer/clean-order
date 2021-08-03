cc5df755d9d17737e68e9ee965de579ec7d0a92e
LucasFlaquer <lucas.flaquer@gmail.com>
2021-08-03 18:12:20 -0300

    place order

---
+++ src/PlaceOrder.ts
@@ -0,0 +1,39 @@
+import { Coupon } from "./Coupon"
+import { Order } from "./Order"
+interface InputItem {
+  description: string
+  price:number
+  quantity: number 
+}
+interface Input {
+  cpf: string
+    items: InputItem[],
+    coupon: string
+}
+
+
+export class PlaceOrder {
+  coupons: Coupon[]
+  orders: Order[]
+  constructor() {
+    this.coupons = [
+      new Coupon("VALE20", 20)
+    ]
+    this.orders = []
+  }
+  execute(input:Input) {
+    const order = new Order(input.cpf)
+    for(const item of input.items) {
+      order.addItem(item.description, item.price, item.quantity)
+    }
+    if(input.coupon) {
+      const coupon = this.coupons.find(coupon => coupon.code === input.coupon) 
+      if(coupon) order.addCoupon(coupon)
+    }
+    const total = order.getTotal()
+    this.orders.push(order)
+    return {
+      total
+    }
+  }
+}
